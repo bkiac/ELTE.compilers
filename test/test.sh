e727a039d5405ee6d1d8a800741b893db1db0f3b
Bence Knáb <benceknab.iac@gmail.com>
2018-04-04 13:01:58 +0200

    tests

---
+++ test/test.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+success=0
+failed=0
+
+for k in ok lexikalis-hibas szintaktikus-hibas szemantikus-hibas
+do
+    for i in `ls $k`
+    do
+        echo ">run $k/$i"
+        ../abap "$k/$i" > /dev/null
+        if [ $? = 0 ];
+        then
+            let success+=1
+        else
+            let failed+=1
+        fi
+    done
+done
+
+echo "ok: $success/5, error: $failed/25"
